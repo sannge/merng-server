0ca7c7e9d0a1e799b5df56ed85d596e7a3d724c8
San Nge <samngestep@gmail.com>
2020-12-01 15:15:56 -0800

    added to deploy server

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://samngestep:aTTKOLtIPgEngh8R@cluster0.fv9yx.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
